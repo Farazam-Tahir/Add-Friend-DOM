a217ff509d5c4d8c83500baad9c3e4049ec9f8f9
Farazam Tahir <frazamtahir28@gmail.com>
2024-02-19 16:20:01 +0500

    Converted the 2 button code into single button

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let addFriend = document.querySelector("#btn2");
-addFriend.addEventListener('click', ()=>{
-    document.querySelector("#status").innerHTML = "Friend";
-});
-
-let removeFriend = document.querySelector("#btn1");
-
+let addRemove = document.querySelector("#addRemove");
 let FriendStatus = document.querySelector("#status");
-    removeFriend.addEventListener('click', ()=>{
-        if (FriendStatus.innerHTML === "Not a Friend" || FriendStatus.innerHTML === "UnFriended") {
-            alert("Not a Friend or already UnFriended");
-        }else{
-            document.querySelector("#status").innerHTML = "UnFriended";
-        }
-    });
+addRemove.addEventListener('click',()=>{
+    if (addRemove.innerHTML == "Add Friend") {
+        addRemove.innerHTML = "Remove";
+        FriendStatus.innerHTML = "Friend";
+    }else{
+        addRemove.innerHTML = "Add Friend";
+        FriendStatus.innerHTML = "Not a Friend";
+    }
+})
+
+// if we have two buttons
+// let addFriend = document.querySelector("#btn2");
+// addFriend.addEventListener('click', ()=>{
+//     document.querySelector("#status").innerHTML = "Friend";
+// });
+
+// let removeFriend = document.querySelector("#btn1");
+
+// let FriendStatus = document.querySelector("#status");
+//     removeFriend.addEventListener('click', ()=>{
+//         if (FriendStatus.innerHTML === "Not a Friend" || FriendStatus.innerHTML === "UnFriended") {
+//             alert("Not a Friend or already UnFriended");
+//         }else{
+//             document.querySelector("#status").innerHTML = "UnFriended";
+//         }
+//     });
